93493c8cdb617479ca48dc3de5519cf08a99fe4f
LeeYunSin <dldbstls7777@naver.com>
2024-01-02 22:01:51 +0900

    chore: supabase 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import MainHeader from "./MainHeader";
+import Form from "./Form";
 
 function App() {
   return (
     <div className="App">
       <MainHeader />
+      <Form />
     </div>
   );
 }
